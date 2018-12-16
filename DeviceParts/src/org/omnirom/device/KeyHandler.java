58f699675e064534792f4588e501635ea56f38e4
micky387 <mickaelsaibi@free.fr>
2018-12-16 07:49:14 +0100

    DeviceParts: unify op6 with op6t
    Change-Id: I580d3ae66d0473a5d908da5979a67d65b32e536c

--- DeviceParts/src/org/omnirom/device/KeyHandler.java
+++ DeviceParts/src/org/omnirom/device/KeyHandler.java
@@ -108,12 +108,14 @@ public class KeyHandler implements DeviceKeyHandler {
     private static final int POCKET_MIN_DELTA_MS = 5000;
     private static final int FP_GESTURE_LONG_PRESS = 305;
 
+    private static final boolean sIsOnePlus6 = android.os.Build.MODEL.equals("ONEPLUS A6003");
+
     public static final String CLIENT_PACKAGE_NAME = "com.oneplus.camera";
     public static final String CLIENT_PACKAGE_PATH = "/data/vendor/omni/client_package_name";
 
     public static final String PACKAGE_SYSTEMUI = "com.android.systemui";
 
-    private static final int[] sSupportedGestures = new int[]{
+    private static final int[] sSupportedGestures6 = new int[]{
         GESTURE_II_SCANCODE,
         GESTURE_CIRCLE_SCANCODE,
         GESTURE_V_SCANCODE,
@@ -129,6 +131,23 @@ public class KeyHandler implements DeviceKeyHandler {
         KEY_SLIDER_CENTER,
         KEY_SLIDER_BOTTOM,
         FP_GESTURE_LONG_PRESS,
+    };
+
+    private static final int[] sSupportedGestures = new int[]{
+        GESTURE_II_SCANCODE,
+        GESTURE_CIRCLE_SCANCODE,
+        GESTURE_V_SCANCODE,
+        GESTURE_A_SCANCODE,
+        GESTURE_LEFT_V_SCANCODE,
+        GESTURE_RIGHT_V_SCANCODE,
+        GESTURE_DOWN_SWIPE_SCANCODE,
+        GESTURE_UP_SWIPE_SCANCODE,
+        GESTURE_LEFT_SWIPE_SCANCODE,
+        GESTURE_RIGHT_SWIPE_SCANCODE,
+        KEY_DOUBLE_TAP,
+        KEY_SLIDER_TOP,
+        KEY_SLIDER_CENTER,
+        KEY_SLIDER_BOTTOM
     };
 
     private static final int[] sProxiCheckedGestures = new int[]{
@@ -344,7 +363,11 @@ public class KeyHandler implements DeviceKeyHandler {
 
     @Override
     public boolean canHandleKeyEvent(KeyEvent event) {
-        return ArrayUtils.contains(sSupportedGestures, event.getScanCode());
+        if (sIsOnePlus6) {
+            return ArrayUtils.contains(sSupportedGestures6, event.getScanCode());
+        } else {
+            return ArrayUtils.contains(sSupportedGestures, event.getScanCode());
+        }
     }
 
     @Override
