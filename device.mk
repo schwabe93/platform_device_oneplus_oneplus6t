5b4ffd99c60e0bc25ec6f248b4aa0dd041b72a06
coderzstas <coderz35@gmail.com>
2019-03-13 15:57:07 +0300

    device : fajita launched with pie

--- device.mk
+++ device.mk
@@ -16,7 +16,7 @@
 #
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/oneplus/sdm845-common/sdm845-common-vendor.mk)
